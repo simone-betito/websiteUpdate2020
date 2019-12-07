8bb88360835e236e76e7212c7d1c217096967055
Simone <betis855@newschool.com>
2019-12-07 17:50:19 -0500

    basic project hover functionality

---
+++ script.js
@@ -0,0 +1,31 @@
+const timeLine = document.querySelectorAll(".year");
+
+timeLine.forEach(year => {
+  const yearID = year.dataset.year;
+
+  year.addEventListener("mouseover", () => {
+    document
+      .querySelectorAll(`.projects-container .y-${yearID}`)
+      .forEach(proj => {
+        proj.classList.add("active");
+      });
+    document
+      .querySelectorAll(`.projects-container > :not(.y-${yearID})`)
+      .forEach(proj => {
+        proj.classList.add("dull");
+      });
+  });
+
+  year.addEventListener("mouseout", () => {
+    document
+      .querySelectorAll(`.projects-container .y-${yearID}`)
+      .forEach(proj => {
+        proj.classList.remove("active");
+      });
+    document
+      .querySelectorAll(`.projects-container > :not(.y-${yearID})`)
+      .forEach(proj => {
+        proj.classList.remove("dull");
+      });
+  });
+});
